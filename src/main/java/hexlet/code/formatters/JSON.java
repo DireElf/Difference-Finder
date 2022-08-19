fbd568f90f8d8012c5fa102db10768bb6cc4e5b5
DireElf <3672@mail.ru>
2022-08-19 15:09:05 +0700

    back

--- src/main/java/hexlet/code/formatters/JSON.java
+++ src/main/java/hexlet/code/formatters/JSON.java
@@ -1,17 +1,16 @@
 package hexlet.code.formatters;
 
-//import com.fasterxml.jackson.core.JsonProcessingException;
-//import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.util.Map;
 
 public class JSON {
     public static String applyJSON(Map<String, String> differences) {
-//        try {
-//            return new ObjectMapper().writeValueAsString(differences);
-//        } catch (JsonProcessingException e) {
-//            throw new RuntimeException(e);
-//        }
-        return "";
+        try {
+            return new ObjectMapper().writeValueAsString(differences);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
